457da3a2f7d832ac3cc56005e4a14e9104f571d8
Kear Ibale <kdwest62@gmail.com>
2025-12-10 09:57:46 +0800

    Implement dice roll counter
    Added a counter to track the number of dice rolls.

--- roll-the-dice.py
+++ roll-the-dice.py
@@ -1,13 +1,15 @@
 import random
 
+counter = 0
 while True:
     roll_dice = input("Roll the dice? (y/n): ")
     if roll_dice == "y" or roll_dice == "Y":
         num1 = random.randint(1, 7)
         num2 = random.randint(1, 7)
         print(f"({num1}, {num2})")
+        counter += 1
     elif roll_dice == "n" or roll_dice == "N":
         break
     else:
         print("Invalid choice!")
-print("Thanks for playing!")
+print(f"Thanks for playing! You rolled the dice {counter} times")
