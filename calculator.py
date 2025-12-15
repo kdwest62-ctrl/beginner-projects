4df71cbdfb83db4fea985ecbe7960ed47919c1c5
Kear Ibale <kdwest62@gmail.com>
2025-12-15 14:01:19 +0800

    Implement error handling for calculator inputs
    Added error handling for invalid input and division by zero.

--- calculator.py
+++ calculator.py
@@ -1,14 +1,19 @@
-num1 = float(input("Input number: "))
-num2 = float(input("Input number: "))
-print("Available operators: + - * /")
-operator = input("Select operator: ")
-if operator ==  "+":
-    print(f"Result: {num1 + num2}")
-elif operator == "-":
-    print(f"Result: {num1 - num2}")
-elif operator == "*":
-    print(f"Result: {num1 * num2}")
-elif operator == "/":
-    print(f"Result: {num1 / num2}")
-else:
-    print("Please select an available operator")
+try:
+    num1 = float(input("Input number: "))
+    num2 = float(input("Input number: "))
+    print("Available operators: + - * /")
+    operator = input("Select operator: ")
+    if operator ==  "+":
+        print(f"Result: {num1 + num2}")
+    elif operator == "-":
+        print(f"Result: {num1 - num2}")
+    elif operator == "*":
+        print(f"Result: {num1 * num2}")
+    elif operator == "/":
+        print(f"Result: {num1 / num2}")
+    else:
+        print("Please select an available operator")
+except ValueError:
+    print("Input a valid number")
+except ZeroDivisionError:
+    print("Cannot divide a number by zero")
