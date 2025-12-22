c3bdec8545c8cf233a9d5e0e30d5d26eb69c1d0f
Kear Ibale <kdwest62@gmail.com>
2025-12-22 11:06:33 +0800

    Enhance calculator with scientific options

--- calculator.py
+++ calculator.py
@@ -1,21 +1,21 @@
-print("1. Simple calculator\n2. Exit")
+print("1. Simple\n2. Scientific\n3. Exit")
 
 while True:
-    choice = input("Enter your choice: ")
+    choice = input("Calculator: ")
     if choice == '1':
         try:
             num1 = float(input("Input number: "))
             num2 = float(input("Input number: "))
-            print("Available operators: + - * /")
+            print("Operators: + - * /")
             operator = input("Select operator: ")
             if operator ==  "+":
-                print(f"Result: {num1 + num2}")
+                print(f"{num1 + num2}")
             elif operator == "-":
-                print(f"Result: {num1 - num2}")
+                print(f"{num1 - num2}")
             elif operator == "*":
-                print(f"Result: {num1 * num2}")
+                print(f"{num1 * num2}")
             elif operator == "/":
-                print(f"Result: {num1 / num2}")
+                print(f"{num1 / num2}")
             else:
                 print("Please select an available operator")
         except ValueError:
@@ -23,6 +23,21 @@ while True:
         except ZeroDivisionError:
             print("Cannot divide a number by zero")
     elif choice == '2':
+        print("1. Power, 2. Square Root, 3. Cube Root")
+        operator = input("Select operator: ")
+        if operator == '1':
+            num = float(input("Input number: "))
+            power = float(input("Power: "))
+            print(num ** power)
+        elif operator == '2':
+            num = float(input("Input number: "))
+            print(num ** (1 / 2))
+        elif operator == '3':
+            num = float(input("Input number: "))
+            print(num ** (1 / 3))
+        else:
+            print("Invalid operator")
+    elif choice == '3':
         print("Program closed")
         break
     else:
