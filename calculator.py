7fbf7288c96b27c2a42793cae33f525f3c2151ad
Kear Ibale <kdwest62@gmail.com>
2025-12-23 10:39:50 +0800

    Improve error handling for calculator inputs
    Added error handling for invalid number inputs in the calculator.

--- calculator.py
+++ calculator.py
@@ -23,20 +23,23 @@ while True:
         except ZeroDivisionError:
             print("Cannot divide a number by zero")
     elif choice == '2':
-        print("1. Power, 2. Square Root, 3. Cube Root")
-        operator = input("Select operator: ")
-        if operator == '1':
-            num = float(input("Input number: "))
-            power = float(input("Power: "))
-            print(num ** power)
-        elif operator == '2':
-            num = float(input("Input number: "))
-            print(num ** (1 / 2))
-        elif operator == '3':
-            num = float(input("Input number: "))
-            print(num ** (1 / 3))
-        else:
-            print("Invalid operator")
+        try:
+            print("1. Power, 2. Square Root, 3. Cube Root")
+            operator = input("Select operator: ")
+            if operator == '1':
+                num = float(input("Input number: "))
+                power = float(input("Power: "))
+                print(num ** power)
+            elif operator == '2':
+                num = float(input("Input number: "))
+                print(num ** (1 / 2))
+            elif operator == '3':
+                num = float(input("Input number: "))
+                print(num ** (1 / 3))
+            else:
+                print("Invalid operator")
+        except ValueError:
+            print("Input a valid number")
     elif choice == '3':
         print("Program closed")
         break
