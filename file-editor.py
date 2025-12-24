f161b2abe83a6a9a0dc1ef6c075cbd393b1e4f7d
Kear Ibale <kdwest62@gmail.com>
2025-12-24 19:00:11 +0800

    Update file editor options and functionality

--- file-editor.py
+++ file-editor.py
@@ -1,27 +1,37 @@
-print("1. Create\n2. Open\n3. Write\n4. Exit")
+print("1. New\n2. Open\n3. Write\n4. Append\n5. Exit")
 
 while True:
-    decide = input("Option: ")
+    decide = input("Choose option: ")
     if decide == '1':
-        filename = input("Filename: ")
-        with open(filename, 'w') as file:
+        filename = input("Enter filename: ")
+        with open(filename, 'x') as file:
             pass
-        print("File created")
+        print(f"{filename} successfully created")
     elif decide == '2':
-        filename = input("Filename: ")
+        filename = input("Enter filename: ")
         with open(filename, 'r') as file:
             print(file.read())
     elif decide == '3':
-        filename = input("Filename: ")
+        filename = input("Enter filename: ")
         lines = int(input("Lines: "))
         count = 1
         while count <= lines:
-            text = input(f"{count}: ")
-            with open(filename, 'a') as file:
+            text = input(f"")
+            with open(filename, 'w') as file:
                 file.write(text + "\n")
             count += 1
         print("File updated")
     elif decide == '4':
+        filename = input("Enter filename: ")
+        lines = int(input("Lines: "))
+        count = 1
+        while count <= lines:
+            text = input(f"")
+            with open(filename, 'a') as file:
+                file.write(text + "\n")
+            count += 1
+        print("File updated")
+    elif decide == '5':
         print("Program closed")
         break
     else:
