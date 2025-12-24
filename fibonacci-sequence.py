81649c4adb0f84b73af1b4347201ec23c9c03d07
Kear Ibale <kdwest62@gmail.com>
2025-12-24 11:02:41 +0800

    Add Fibonacci sequence generator
    Implement Fibonacci sequence generator based on user input.

---
+++ fibonacci-sequence.py
@@ -0,0 +1,13 @@
+fibonacci = [0, 1]
+sequence = int(input("Input sequence: "))
+if sequence <= 0:
+    print("Invalid input")
+elif sequence == 1:
+    print(f"{fibonacci[0]}")
+elif sequence == 2:
+    print(f"{fibonacci}")
+else:
+    while len(fibonacci) != sequence:
+        num = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(num)
+    print(fibonacci)
