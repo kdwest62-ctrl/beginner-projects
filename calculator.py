760d1292dc565c6d5f16af9831802317e65e4dfa
Kear Ibale <kdwest62@gmail.com>
2025-12-28 10:41:22 +0800

    Add factorial calculation to scientific options

--- calculator.py
+++ calculator.py
@@ -1,13 +1,14 @@
-print("1. Simple\n2. Scientific\n3. Exit")
+import math
 
+print("1. Simple\n2. Scientific\n3. Exit")
 while True:
     choice = input("Calculator: ")
     if choice == '1':
         try:
-            num1 = float(input("Input number: "))
-            num2 = float(input("Input number: "))
             print("Operators: + - * /")
             operator = input("Select operator: ")
+            num1 = float(input("Input number: "))
+            num2 = float(input("Input number: "))
             if operator ==  "+":
                 print(f"{num1 + num2}")
             elif operator == "-":
@@ -24,18 +25,18 @@ while True:
             print("Cannot divide a number by zero")
     elif choice == '2':
         try:
-            print("1. Power, 2. Square Root, 3. Cube Root")
+            print("1. Power, 2. Square Root, 3. Cube Root, 4. Factorial")
             operator = input("Select operator: ")
+            num = float(input("Input number: "))
             if operator == '1':
-                num = float(input("Input number: "))
                 power = float(input("Power: "))
                 print(num ** power)
             elif operator == '2':
-                num = float(input("Input number: "))
                 print(num ** (1 / 2))
             elif operator == '3':
-                num = float(input("Input number: "))
                 print(num ** (1 / 3))
+            elif operator == '4':
+                print(math.factorial(num))
             else:
                 print("Invalid operator")
         except ValueError:
@@ -44,4 +45,4 @@ while True:
         print("Program closed")
         break
     else:
-        print("Please enter a valid choice")
+        print("Invalid choice")
