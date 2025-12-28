e3f6f815a5060aa901283d7c45eba0345634c4bd
Kear Ibale <kdwest62@gmail.com>
2025-12-28 10:26:16 +0800

    Improve bulls and cows logic with dictionary comparison
    Refactor guessing logic to use dictionaries for bulls and cows calculation.

--- cows-n-bulls.py
+++ cows-n-bulls.py
@@ -1,24 +1,25 @@
 import random
 
 rand_num = str(random.randint(1000, 9999))
+rand_dict = dict(enumerate(rand_num))
 attempt = 0
-
 while True:
     bulls = 0
     cows = 0
     guess_num = input("Guess the number: ")
-    if guess_num == rand_num:
+    guess_dict = dict(enumerate(guess_num))
+    if guess_dict == rand_dict:
         attempt += 1
         print(f"Well done! You have guessed the number. You did it in {attempt} attempts")
         break
     else:
-        for item in guess_num:
-            if item in rand_num:
-                if guess_num.index(item) == rand_num.index(item):
+        attempt += 1
+        for key in guess_dict:
+            if guess_dict[key] in rand_dict.values():
+                if guess_dict[key] == rand_dict[key]:
                     bulls += 1
                 else:
                     cows += 1
             else:
                 continue
-        attempt += 1
         print(f"Cows: {cows}, Bulls: {bulls}")
