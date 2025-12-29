ddad78d86a6215de947ffa82d96d574e404f48f8
Kear Ibale <kdwest62@gmail.com>
2025-12-29 10:16:30 +0800

    Add bet validation and adjust game flow

--- slot-machine.py
+++ slot-machine.py
@@ -23,23 +23,24 @@ while True:
         print("Game over")
         break
     bet = int(input("Enter your bet amount: $"))
-    num1 = random.randint(1, 6)
-    num2 = random.randint(1, 6)
-    num3 = random.randint(1, 6)
-    print(f"{num1} | {num2} | {num3}")
-    winnings = bet_multiply(num1, num2, num3, bet)
-    print(f"You won ${winnings}")
-    if winnings == 0:
+    if bet > current_bal:
+        print("Your bet can't be higher than your current balance!")
+    else:
         current_bal -= bet
-    else:
-        current_bal += (bet_multiply(num1, num2, num3, bet))
-    if current_bal == 0:
-        print(f"You currently have ${current_bal} balance")
-        print("Game over")
-        break
-    else:
-        user_decide = input("Do you want to play again? (y/n): ")
-        if user_decide == "n":
+        num1 = random.randint(1, 6)
+        num2 = random.randint(1, 6)
+        num3 = random.randint(1, 6)
+        print(f"{num1} | {num2} | {num3}")
+        winnings = bet_multiply(num1, num2, num3, bet)
+        print(f"You won ${winnings}")
+        if winnings != 0:
+            current_bal += (bet_multiply(num1, num2, num3, bet))
+        if current_bal == 0:
+            print(f"You currently have ${current_bal} balance")
+            print("Game over")
             break
-
+        else:
+            user_decide = input("Do you want to play again? (y/n): ")
+            if user_decide == "n":
+                break
 print("Thank you for playing")
