60cd2de15b6da433c98cd0f29ce6adadf2e08e27
Kear Ibale <kdwest62@gmail.com>
2026-01-11 11:47:38 +0800

    Refactor word selection and guessing logic

--- word-guess.py
+++ word-guess.py
@@ -1,43 +1,31 @@
+import random
+
 keys = []
-words = {'Easy': 'set', 'Medium': 'tuple', 'Hard': 'module'}
-print("1. Easy, 2. Medium, 3. Hard")
-choice = input("Select difficulty: ")
-if choice == '1':
-    word = words['Easy']
-    for letter in word:
-        keys.append(letter)
-elif choice == '2':
-    word = words['Medium']
-    for letter in word:
-        keys.append(letter)
-elif choice =='3':
-    word = words['Hard']
-    for letter in word:
-        keys.append(letter)
-else:
-    print("Invalid input")
+words = ['string', 'tuples', 'module', 'random']
+word = random.choice(words)
+for letter in word:
+    keys.append(letter)
 
-if len(keys) != 0:
-    values = []
-    count = 0
-    while count != len(keys):
-        values.append('_')
-        count += 1
-    word_to_guess = dict(zip(keys, values))
+values = []
+count = 0
+while count != len(keys):
+    values.append('_')
+    count += 1
+word_to_guess = dict(zip(keys, values))
 
-    attempt = 0
-    score = 0
-    while score != len(word_to_guess):
-        print(list(word_to_guess.values()))
-        guess = input("Guess a letter: ")
-        if guess in word_to_guess.keys():
-            print("Correct guess")
-            attempt += 1
-            score += 1
-            update = {guess: guess}
-            word_to_guess.update(update)
-        else:
-            print("Wrong guess")
-            attempt += 1
+attempt = 0
+score = 0
+while score != len(word_to_guess):
     print(list(word_to_guess.values()))
-    print(f"Well done! You have guessed the word\nYou did it in {attempt} attempts")
+    guess = input("Guess a letter: ")
+    if guess in word_to_guess.keys():
+        print("Correct guess")
+        attempt += 1
+        score += 1
+        update = {guess: guess}
+        word_to_guess.update(update)
+    else:
+        print("Wrong guess")
+        attempt += 1
+print(list(word_to_guess.values()))
+print(f"Well done! You have guessed the word\nYou did it in {attempt} attempts")
