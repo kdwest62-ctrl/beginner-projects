7110634a868ddbdea915116e5a7988ab8db46784
Kear Ibale <kdwest62@gmail.com>
2026-01-12 10:40:01 +0800

    Refactor word guessing game for difficulty selection

--- word-guess.py
+++ word-guess.py
@@ -1,31 +1,40 @@
-import random
-
 keys = []
-words = ['string', 'tuples', 'module', 'random']
-word = random.choice(words)
-for letter in word:
-    keys.append(letter)
+words = ['list', 'tuple', 'module']
+print("1. Easy, 2. Medium, 3. Hard")
+difficulty = input("Select difficulty: ")
+if difficulty == '1':
+    for letter in words[0]:
+        keys.append(letter)
+elif difficulty == '2':
+    for letter in words[1]:
+        keys.append(letter)
+elif difficulty == '3':
+    for letter in words[2]:
+        keys.append(letter)
+else:
+    print("Invalid input")
 
-values = []
-count = 0
-while count != len(keys):
-    values.append('_')
-    count += 1
-word_to_guess = dict(zip(keys, values))
+if len(keys) != 0:
+    values = []
+    count = 0
+    while count != len(keys):
+       values.append('-')
+       count += 1
+    word_to_guess = dict(zip(keys, values))
 
-attempt = 0
-score = 0
-while score != len(word_to_guess):
-    print(list(word_to_guess.values()))
-    guess = input("Guess a letter: ")
-    if guess in word_to_guess.keys():
-        print("Correct guess")
-        attempt += 1
-        score += 1
-        update = {guess: guess}
-        word_to_guess.update(update)
-    else:
-        print("Wrong guess")
-        attempt += 1
-print(list(word_to_guess.values()))
-print(f"Well done! You have guessed the word\nYou did it in {attempt} attempts")
+    attempt = 0
+    score = 0
+    while score != len(word_to_guess):
+       print("".join(word_to_guess.values()))
+       guess = input("Guess a letter: ")
+       if guess in word_to_guess.keys():
+           print("Correct guess")
+           attempt += 1
+           score += 1
+           update = {guess: guess}
+           word_to_guess.update(update)
+       else:
+           print("Wrong guess")
+           attempt += 1
+    print("".join(word_to_guess.values()))
+    print(f"Well done! You have guessed the word\nYou did it in {attempt} attempts")
