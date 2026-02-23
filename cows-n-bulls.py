e71890e8af52a80dfbd0e3eb3622a3db754dfffd
Kear Ibale <kdwest62@gmail.com>
2026-02-23 19:54:57 +0800

    Improve random number generation and guessing mechanics
    Refactor random number generation and guessing logic with error handling.

--- cows-n-bulls.py
+++ cows-n-bulls.py
@@ -1,37 +1,43 @@
 import random
 
-rand_list = []
-length = int(input("Length of random number: "))
-count = 0
-while length != count:
-    num = random.randint(0, 9)
-    rand_list.append(str(num))
-    count += 1
-rand_num = "".join(rand_list)
-rand_dict = dict(enumerate(rand_num))
+try:
+    length = int(input("Length of random number: "))
+    if length != 0:
+        digits = []
+        count = 0
+        while count != length:
+            num = random.randint(0, 9)
+            digits.append(str(num))
+            count += 1
+        rand_num = "".join(digits)
+        rand_num_with_index = dict(enumerate(rand_num))
 
-attempt = 0
-incorrect = 0
-while True:
-    bulls = 0
-    cows = 0
-    guess_num = input(f"Guess the {length}-digit number: ")
-    guess_dict = dict(enumerate(guess_num))
-    if guess_dict == rand_dict:
-        attempt += 1
-        print(f"Well done! You have guessed the number. You did it in {attempt} attempts")
-        break
-    else:
-        attempt += 1
-        incorrect += 1
-        for key in guess_dict:
-            if guess_dict[key] in rand_dict.values():
-                if guess_dict[key] == rand_dict[key]:
-                    bulls += 1
-                else:
-                    cows += 1
+        attempt = 0
+        wrong_guess = 0
+        while True:
+            bulls = 0
+            cows = 0
+            guess_num = input(f"Guess the {length}-digit number: ")
+            if len(guess_num) > length:
+                print("Invalid length")
+                attempt += 1
             else:
-                continue
-        print(f"Cows: {cows}, Bulls: {bulls}")
-    if incorrect == 5:
-        print(f"Hint: first digit = {rand_dict[0]}")
+                guess_with_index = dict(enumerate(guess_num))
+                if guess_num == rand_num:
+                    attempt += 1
+                    print(f"Well done! You have guessed the number\nIt took {attempt} attempts")
+                    break
+                else:
+                    attempt += 1
+                    wrong_guess += 1
+                    for digit in guess_with_index:
+                        if guess_with_index[digit] in rand_num_with_index.values():
+                            if guess_with_index[digit] == rand_num_with_index[digit]:
+                                bulls += 1
+                            else:
+                                cows += 1
+                    print(f"Cows: {cows}, Bulls: {bulls}")
+                if wrong_guess == 5:
+                    print(f"Hint: first digit = {rand_num_with_index[0]}")
+except ValueError:
+    print("Please input a number")
