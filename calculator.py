f0a18ca9723832195863b09d2f88a307707491e7
Kear Ibale <kdwest62@gmail.com>
2026-07-09 07:33:57 +0800

    Refactor calculator into a class-based structure
    Refactored calculator functionality into a Calculator class with methods for addition, subtraction, multiplication, and division.

--- calculator.py
+++ calculator.py
@@ -1,48 +1,12 @@
-import math
-
-print("1. Simple\n2. Scientific\n3. Exit")
-while True:
-    choice = input("Calculator: ")
-    if choice == '1':
-        try:
-            print("Operators: + - * /")
-            operator = input("Select operator: ")
-            num1 = float(input("Input number: "))
-            num2 = float(input("Input number: "))
-            if operator ==  "+":
-                print(f"{num1 + num2}")
-            elif operator == "-":
-                print(f"{num1 - num2}")
-            elif operator == "*":
-                print(f"{num1 * num2}")
-            elif operator == "/":
-                print(f"{num1 / num2}")
-            else:
-                print("Please select an available operator")
-        except ValueError:
-            print("Input a valid number")
-        except ZeroDivisionError:
-            print("Cannot divide a number by zero")
-    elif choice == '2':
-        try:
-            print("1. Power, 2. Square Root, 3. Cube Root, 4. Factorial")
-            operator = input("Select operator: ")
-            num = float(input("Input number: "))
-            if operator == '1':
-                power = float(input("Power: "))
-                print(num ** power)
-            elif operator == '2':
-                print(num ** (1 / 2))
-            elif operator == '3':
-                print(num ** (1 / 3))
-            elif operator == '4':
-                print(math.factorial(num))
-            else:
-                print("Invalid operator")
-        except ValueError:
-            print("Input a valid number")
-    elif choice == '3':
-        print("Program closed")
-        break
-    else:
-        print("Invalid choice")
+class Calculator:
+  def __init__(self, num1, num2):
+    self.num1 = num1
+    self.num2 = num2
+  def add(self):
+    return self.num1 + self.num2
+  def subtract(self):
+    return self.num1 - self.num2
+  def multiply(self):
+    return self.num1 * self.num2
+  def divide(self):
+    return self.num1 / self.num2
